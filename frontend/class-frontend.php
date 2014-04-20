7e8fbc4c1aa5bf45bbaa3e43dac04bf4457bd86d
Kevin Weber <k-iwi@web.de>
2014-04-20 16:49:33 +0200

    Enqueued jQuery

--- frontend/class-frontend.php
+++ frontend/class-frontend.php
@@ -5,11 +5,19 @@
 class INCOM_Frontend {
 
 	function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'lazyload_enqueue_jquery' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'incom_enqueue_scripts' ) );
 		add_action( 'wp_footer', array( $this, 'load_incom'), 15, 'functions' );
 		add_action( 'wp_enqueue_scripts', array( $this, 'load_incom_style') );
 		add_action( 'wp_head', array( $this, 'load_incom_custom_css') );
 	}
+
+	/**
+ 	 * Enable jQuery (comes with WordPress)
+ 	 */
+ 	function lazyload_enqueue_jquery() {
+     	wp_enqueue_script( 'jquery' );
+ 	}
 
 	function incom_enqueue_scripts() {}
 	function load_incom() {}
