10c6ab05404c1083aec9f01256a3dab42c167592
Kevin Weber <k-iwi@web.de>
2014-05-12 22:20:40 +0200

    Added signup form to plugin activation page; added styling

--- admin/class-admin-options.php
+++ admin/class-admin-options.php
@@ -192,33 +192,16 @@ class INCOM_Admin_Options {
 			    <?php submit_button(); ?>
 			</form>
 
-
-			<?php if ( INCOM_DISQUS || INCOM_ESSENTIAL ) { ?>
-			<div id="mc_embed_signup">
-			<form action="http://kevinw.us2.list-manage.com/subscribe/post?u=f65d804ad274b9c8812b59b4d&amp;id=20c3ab10d8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				<h2>Get Free Updates</h2>
-			<div class="mc-field-group">
-				<label for="mce-EMAIL">Email Address </label>
-				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-				<input type="hidden" name="GROUPS" id="GROUPS" value="<?= INCOM_VERSION_NAME; ?>" />
-			</div>
-				<div id="mce-responses" class="clear">
-					<div class="response" id="mce-error-response" style="display:none"></div>
-					<div class="response" id="mce-success-response" style="display:none"></div>
-				</div>
-			    <div style="position: absolute; left: -5000px;"><input type="text" name="b_f65d804ad274b9c8812b59b4d_20c3ab10d8" tabindex="-1" value=""></div>
-			    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-			</form>
-			</div>
-			<?php } ?>
-
+			<?php if ( INCOM_ESSENTIAL ) {
+				require_once( 'inc/signup.php' );
+			} ?>
 
 		    <table class="form-table">
 		        <tr valign="top">
 		        <th scope="row" style="width:100px;"><a href="http://kevinw.de/ic" target="_blank"><img src="http://www.gravatar.com/avatar/9d876cfd1fed468f71c84d26ca0e9e33?d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536&s=100" style="-webkit-border-radius:50%;-moz-border-radius:50%;border-radius:50%;"></a></th>
 		        <td style="width:200px;">
 		        	<p><a href="http://kevinw.de/ic" target="_blank">Kevin Weber</a> &ndash; that's me.<br>
-		        	I'm the developer of this plugin. I hope you enjoy it!</p></td>
+		        	I'm the developer of this plugin. Love it!</p></td>
 		        <?php if ( INCOM_DISQUS ) { ?>
 		        <td>
 					<p><b>It's free!</b> Support me with <a href="http://kevinw.de/donate/InlineComments/" title="Pay me a delicious lunch" target="_blank">a delicious lunch</a> or give this plugin a 5 star rating <a href="http://wordpress.org/support/view/plugin-reviews/inline-comments?filter=5" title="Vote for Inline Comments" target="_blank">on WordPress.org</a>.</p>
--- admin/class-register.php
+++ admin/class-register.php
@@ -8,8 +8,30 @@ register_activation_hook( INCOM_FILE, 'incom_plugin_activation' );
 register_deactivation_hook( INCOM_FILE, 'incom_plugin_deactivation' );
 
 function incom_plugin_activation() {
+	$signup = '';
+	if ( INCOM_ESSENTIAL ) {
+		$signup = '<div id="mc_embed_signup">
+				<form action="http://kevinw.us2.list-manage.com/subscribe/post?u=f65d804ad274b9c8812b59b4d&amp;id=20c3ab10d8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div class="mc-field-group">
+						<label for="mce-EMAIL" style="line-height:2.5em">The only way to get notified when an update is available for Inline Comments:</label><br>
+						<input type="email" value="Enter your email address" name="EMAIL" class="required email" id="mce-EMAIL" onclick="this.focus();this.select()" onfocus="if(this.value == \'\') { this.value = this.defaultValue; }" onblur="if(this.value == \'\') { this.value = this.defaultValue; }">
+						<input type="hidden" name="GROUPS" id="GROUPS" value="'.INCOM_VERSION_NAME.'" />
+						<input type="submit" value="Get free updates" name="subscribe" id="mc-embedded-subscribe" class="button">
+					</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>
+				    <div style="position: absolute; left: -5000px;"><input type="text" name="b_f65d804ad274b9c8812b59b4d_20c3ab10d8" tabindex="-1" value=""></div>
+				</form>
+				</div>';
+	}
+
 	$notices = get_option( 'incom_deferred_admin_notices', array() );
-	$notices[] = 'Edit your plugin settings: <strong><a href="options-general.php?page=incom.php">Inline Comments</a></strong>';
+	$notices[] = 'Edit your plugin settings: <strong>
+					<a href="options-general.php?page=incom.php">Inline Comments</a>
+					</strong>' . $signup;
+				;
 	update_option( 'incom_deferred_admin_notices', $notices );
 }
 
