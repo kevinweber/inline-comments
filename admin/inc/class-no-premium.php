62fa287e8efc0bf111e6df4bec65bf0e7154adfd
kevinweber <k-iwi@web.de>
2014-10-19 02:07:49 +0200

    Updated no premium tab

--- admin/inc/class-no-premium.php
+++ admin/inc/class-no-premium.php
@@ -35,42 +35,38 @@ class INCOM_No_Premium_Admin_Options {
 	function add_incom_admin_tab() { ?>
 		<div id="premium">
 
-			<h3>Get Premium and &hellip;</h3>
+			<h3>Premium Extension</h3>
+
+			<p>Right now, every feature for Inline Comments is available FOR FREE, except for the "Remove branding" option. This is what you get when you buy premium:</p>
 
 			<table class="form-table">
 				<tbody>
 			        <tr valign="top">
-			        	<th scope="row">&hellip; remove branding</th>
+			        	<th scope="row">Remove branding</th>
 				        <td>
-							<span>The <i>Premium Extension</i> automatically removes the branding link from Inline Comments.</span>
+							<span>The premium extension automatically removes the subtle branding link.</span>
 				        </td>
 			        </tr>
 			        <tr valign="top">
-			        	<th scope="row">&hellip; display avatars</th>
-				        <td>
-							<span>Display photos/avatars from commentators next to each comment.</span>
-				        </td>
-			        </tr>
-			        <tr valign="top">
-			        	<th scope="row">&hellip; insert content</th>
-				        <td>
-							<span>Insert content (any HTML) above the list of comments.</span>
-				        </td>
-			        </tr>
-			        <tr valign="top">
-			        	<th scope="row">&hellip; get setup support</th>
+			        	<th scope="row">Get setup support</th>
 				        <td>
 							<span>I help you to choose the correct selectors.</span>
 				        </td>
 			        </tr>
 			        <tr valign="top">
-			        	<th scope="row">&hellip; request features</th>
+			        	<th scope="row">Push development</th>
+				        <td>
+							<span>This plugin does not anywhere near compensate my month-long time efforts financially. WordPress development is a hobby of mine and allows me to experiment. With your purchase, you sponsor me to spend at least a bit more time to enhance Inline Comments.</span>
+				        </td>
+			        </tr>
+			        <tr valign="top">
+			        	<th scope="row">Request features</th>
 				        <td>
 							<span>I'm going to develop your feature request with priority (but no guarantee).</span>
 				        </td>
 			        </tr>
 			        <tr valign="top">
-			        	<th scope="row">&hellip; enjoy coming features<br><span class="description thin">with free lifetime updates!</span></th>
+			        	<th scope="row">Enjoy coming features<br><span class="description thin">with free lifetime updates!</span></th>
 				        <td>
 							<span>No matter what comes next: Once you've bought premium, you're going to get every new feature for free. What do you think of social logins (Twitter, Facebook) or Inline Comments for Editors?</span>
 				        </td>
